93ab23c17b56170e9dedccac63e245ea1be5e8b8
Elliot Betancourt <elliot@elliotbetancourt.com>
2019-03-14 12:16:04 +0100

    update frontend

--- blueprints/userspace.py
+++ blueprints/userspace.py
@@ -6,4 +6,4 @@ userspace_blueprint = flask.Blueprint('userspace_blueprint', __name__)
 @userspace_blueprint.route('/')
 @userspace_blueprint.route('/test')
 def index():
-    return 'view your cards here'
+    return flask.render_template('cards.html', message='view your cards here')
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,19 +2,12 @@ window.jQuery = require('jquery');
 window.$ = jQuery;
 window.Tether = require('tether');
 window.bootstrap = require('bootstrap');
-// var _ = require('lodash');
-// var Rickshaw = require('rickshaw');
-// var d3 = require('d3js');
-// var nvd3 = require('nvd3js');
-// var moment = require('moment');
-// require('datejs');
-// require('select2');
+
+// not using React in this project yet
+// import React from 'react';
+// import { render } from 'react-dom';
+// import 'react-bootstrap';
+// import App from './components/App';
 
 
-import React from 'react';
-import { render } from 'react-dom';
-import 'react-bootstrap';
-import App from './components/App';
-
-
-render(<App />, document.querySelector('#main'));
+// render(<App />, document.querySelector('#main'));
