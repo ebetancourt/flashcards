03522de87e1ef25d33a8c3dc07584b4102e5f25b
Elliot Betancourt <elliot@elliotbetancourt.com>
2019-03-14 13:13:49 +0100

    remove SQLALCHEMY_TRACK_MODIFICATIONS warnings

--- config/config.py
+++ config/config.py
@@ -3,3 +3,4 @@ import os
 DEBUG = True
 SECRET_KEY = 'REPLACE ME - this value is here as a placeholder.'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False
