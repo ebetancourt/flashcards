8d96d001efeea286bf3fd3c01d8352b5d267e9dc
Elliot Betancourt <elliot@elliotbetancourt.com>
2019-03-16 15:38:02 +0100

    start working on card saving

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,4 +1,6 @@
 import flask
+from flask import request
+from models.Card import Card
 from flask_user import current_user, login_required, roles_required
 
 admin_blueprint = flask.Blueprint('admin_blueprint', __name__)
@@ -9,3 +11,25 @@ admin_blueprint = flask.Blueprint('admin_blueprint', __name__)
 @login_required
 def index():
     return 'admin area. Edit cards here'
+
+
+@admin_blueprint.route('/new')
+@login_required
+def create_card():
+    return flask.render_template('admin/card_form.html', card=Card())
+
+
+@admin_blueprint.route('/save', methods=['POST'])
+@login_required
+def save_card():
+    if request.method == 'POST':
+        if 'id' in request.form:
+            card = Card.find(request.form['id'])
+        else:
+            card = Card()
+
+        card.word = request.form['word']
+        card.definition = request.form['definition']
+        card.save()
+
+    return flask.render_template('admin/card_form.html', card=card)
